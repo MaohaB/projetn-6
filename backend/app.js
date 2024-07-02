3f198c9d9c755a04a102f5fbe6bc4805007c9e0d
MaohaB <leila.posonsby@gmail.com>
2024-07-02 16:26:04 +0200

    Mongodb

--- backend/app.js
+++ backend/app.js
@@ -2,7 +2,12 @@ const express = require('express');
 const app = express();
 const PORT = 4000;
 const cors = require("cors");
-//const mongoose = require('mongoose');
+const mongoose = require('mongoose');
+mongoose.connect('mongodb+srv://jimbob:<PASSWORD>@cluster0-pme76.mongodb.net/test?retryWrites=true&w=majority',
+    { useNewUrlParser: true,
+      useUnifiedTopology: true })
+    .then(() => console.log('Connexion à MongoDB réussie !'))
+    .catch(() => console.log('Connexion à MongoDB échouée !'));
 
 
 app.use(cors());
