529ffc5e23bfcb2a6974f16115873e40df013f63
MaohaB <leila.posonsby@gmail.com>
2024-07-02 16:04:42 +0200

    sign up + login

--- backend/app.js
+++ backend/app.js
@@ -1,10 +1,33 @@
 const express = require('express');
 const app = express();
-console.log("app", app)
+const PORT = 4000;
+const cors = require("cors");
+//const mongoose = require('mongoose');
 
-app.get('/', function (req, res) {
-    res.send("Hello World!");
-  });
+
+app.use(cors());
+app.use(express.json());
+
+//app.get('/', function (req, res) { res.send("Hello World!");});
   
-  app.listen(3000);
-  
+app.listen(PORT, function(err){
+    if (err) console.log("Error in server setup")
+    console.log("Server listening on Port", PORT);
+})
+
+app.post('/api/auth/signup', UserSignup) ; 
+function UserSignup(req,res) {
+const body = req.body;
+console.log("body", body)
+// message string
+res.send("sign up");
+};
+app.post('/api/auth/login', UserLogin) ;
+function UserLogin(req,res) {
+const body = req.body;
+console.log("body", body)
+// userID + token
+res.send({
+    userId: "string",
+    token: "string"});
+};
