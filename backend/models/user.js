2890ab4a301a1817936c379b91a0ca2c543bfb33
MaohaB <leila.posonsby@gmail.com>
2024-07-19 11:38:22 +0200

    correction connexion mongo

--- backend/models/user.js
+++ backend/models/user.js
@@ -1,16 +1,5 @@
 const mongoose = require('mongoose');
-
-const PASSWORD = "saQXm4NsrcMqU9vY";
-const USER = "maoh_";
-//const URL_DB = `mongodb+srv://$(USER):$(PASSWORD)@cluster0.x5n4n7y.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
-const URL_DB = 'mongodb+srv://maoh_:saQXm4NsrcMqU9vY@cluster0.x5n4n7y.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0'
-mongoose.connect( URL_DB,
-    { useNewUrlParser: true,
-      useUnifiedTopology: true })
-    .then(() => console.log('Connexion à MongoDB réussie !'))
-    .catch(() => console.log('Connexion à MongoDB échouée !'));
-
-
+const mongo = require('../mongodb/mongo');
 
 
     const userSchema = mongoose.Schema({
--- backend/mongodb/mongo.js
+++ backend/mongodb/mongo.js
@@ -2,39 +2,12 @@ const mongoose = require('mongoose');
 
 const PASSWORD = "saQXm4NsrcMqU9vY";
 const USER = "maoh_";
-//const URL_DB = `mongodb+srv://$(USER):$(PASSWORD)@cluster0.x5n4n7y.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
-const URL_DB = 'mongodb+srv://maoh_:saQXm4NsrcMqU9vY@cluster0.x5n4n7y.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0'
+const URL_DB = `mongodb+srv://${USER}:${PASSWORD}@cluster0.x5n4n7y.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
+//const URL_DB = 'mongodb+srv://maoh_:saQXm4NsrcMqU9vY@cluster0.x5n4n7y.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0'
 mongoose.connect( URL_DB,
     { useNewUrlParser: true,
       useUnifiedTopology: true })
     .then(() => console.log('Connexion à MongoDB réussie !'))
     .catch(() => console.log('Connexion à MongoDB échouée !'));
 
-
-
-
-    const userSchema = mongoose.Schema({
-        email : String , //adresse e-mail de l’utilisateur [unique]
-        password : String //mot de passe haché de l’utilisateur
-    });
-    
-    
-    
-    const User = mongoose.model("User", userSchema);
-    
-    module.exports = {User}
-
-
-    // const userSchema = mongoose.Schema({
-    //     email : String , //adresse e-mail de l’utilisateur [unique]
-    //     password : String //mot de passe haché de l’utilisateur
-    // });
-    
-    // const User = mongoose.model("User", userSchema);
-    
-    // const user = new User({
-    //     email : email,
-    //     password : password
-    //  })
-    
-    // user.save().then(() => console.log("saved"));
+module.exports = {URL_DB}
