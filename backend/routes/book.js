589795e28a5bd2c67d216aaee3d8f2a2ed15b17b
MaohaB <leila.posonsby@gmail.com>
2024-07-26 10:41:08 +0200

    postbook authorized res 403

--- backend/routes/book.js
+++ backend/routes/book.js
@@ -1,11 +1,15 @@
 const express = require('express');
 const { postBook, getBooks, getBookbyID, getBestRating} = require("../controllers/book.js")
 const {upload} = require('../middleware/multer.js')
+const auth = require('../middleware/auth');
 
 const booksRouter = express.Router();
 booksRouter.get("/", getBooks);
 booksRouter.get("/bestrating", getBestRating);
 booksRouter.get("/:id", getBookbyID);
-booksRouter.post("/",upload.single("image"), postBook);
+booksRouter.post("/", auth, upload.single("image"), postBook);
+//booksRouter.delete("/:id",auth, deleteBook);
+//booksRouter.put("/:id", auth,updateBook);
+//booksRouter.post("/:id/rating",auth, rateBookbyID);
 
 module.exports = { booksRouter};
